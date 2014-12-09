b1d0b699accd8c11516db331c9ad33ffc2ffc25a
Josh Heyer <josh@hyr.io>
2014-12-09 07:45:53 -0700

    Update class to a PM::FormScreen and add some form elements

--- app/screens/new_debt_screen.rb
+++ app/screens/new_debt_screen.rb
@@ -1,17 +1,75 @@
-class NewDebtScreen < PM::Screen
+class NewDebtScreen < PM::FormScreen
+  title "Track Another Debt"
   stylesheet NewDebtStylesheet
 
   def on_load
-    set_nav_bar_button :right, title: "Save", action: :done, type: :done
-
-    btn = append!(UIButton, :close_modal).on(:tap) do |x|
-     done
-    end
+    set_nav_bar_button :right, title: "Save", action: :save, type: :save
   end
 
-  def done
-    PM.logger.info "Closing Screen"
+  def form_data
+    [{
+      title: "Debt Details",
+      footer: "Some help text",
+      cells: [{
+        name: "debt_name",
+        title: "Who is the debt with?",
+        type: :text,
+        vallue: ""
+      },
+      {
+        name: "debt_balance",
+        title: "What is the current balance?",
+        type: :float,
+        value: ""
+      },
+      {
+        name: "debt_interest_rate",
+        title: "What is the interest rate?",
+        type: :float,
+        value: ""
+      },
+      {
+        name: "debt_minimum_amount",
+        title: "What is the minimum amount?",
+        type: :float,
+        value: ""
+      },
+      {
+        name: "debt_due_date",
+        title: "What is the next due date?",
+        type: :text,
+        value: ""
+      }]
+    }]
+  end
+
+  def save
+    PM.logger.info "Save and close screen."
     close
   end
 
 end
+
+
+# class MyFormScreen < PM::FormScreen
+#   title "My Form"
+
+#   def form_data
+#     [{
+#       title: "Account Information",
+#       footer: "Some help text",
+#       cells: [{
+#         name: "email",
+#         title: "ID",
+#         type: :email,
+#         value: current_user.email,
+#       }, {
+#         name: "password",
+#         title: "New Password",
+#         type: :password,
+#         value: ""
+#       }]
+#     }]
+#   end
+
+# end
